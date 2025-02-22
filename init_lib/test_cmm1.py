01a7fb3bae6418fd2c61640bd80cb1ef8e595c3d
Wenrui Huang <rijuyuezhuhwr@gmail.com>
2025-02-23 01:43:32 +0800

    feat(*): delete unnecessary test files

--- init_lib/test_cmm1.py
+++
@@ -1,7 +0,0 @@
-from init_lib.utils import tupleassert, rtest
-
-
-class TestSeperated:
-    def cmm1(self):
-        tupleassert(
-            rtest("./build/src/parser ./Original-Lab/Test/test1.cmm"), ("", ""))
